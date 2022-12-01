f16d5b610153350dd196207b981faf93c9aa3211
mati <matiexe31@gmail.com>
2022-12-01 15:17:03 -0300

    se agregan los export a example.jsx y app.jsx ubicado en routes , en app se agrega el componente example a una de las rutas

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -6,3 +6,5 @@ const Example = () =>{
         <Example/>
     )
 }
+
+export default Example
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Example from '../pages/Example';
 
-const App () =>{
+const App = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                <Route exact path = '/' element = {}/>
-                <Route exact path = '/example' element = {}/>
-                <Route exact path = '*' element = {}/>
+                {/* <Route exact path = '/' element = {}/> */}
+                <Route exact path = '/example' element = {<Example/>}/>
+                <Route exact path = '*' element = "NOT FOUND" />
             </Routes>
         </BrowserRouter>
     )
 }
+
+export default App;
