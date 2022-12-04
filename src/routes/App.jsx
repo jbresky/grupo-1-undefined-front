f6cef87eacc7406db81325f4cdc573f95507d6d7
Joaquin <93297507+jbresky@users.noreply.github.com>
2022-12-04 20:55:18 -0300

    Componente Header (#3)
    * installing chakra UI and necessary dependencies
    
    * app wrapped with chakra provider
    
    * react-icons installed: package for use icons as components
    
    * wrapping app in routes folder
    
    * Header component: heading and interactive menu
    
    * Drawers folder: drawers son barras de menu desplegables, hice una carpeta en caso de necesitar mas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Example from '../pages/Example';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
-const App = () =>{
-    return(
-        <BrowserRouter>
-            <Routes>
-                {/* <Route exact path = '/' element = {}/> */}
-                <Route exact path = '/example' element = {<Example/>}/>
-                <Route exact path = '*' element = "NOT FOUND" />
-            </Routes>
-        </BrowserRouter>
+const App = () => {
+    return (
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={"Home page... nothing to see yet"} />
+                    <Route exact path='*' element="NOT FOUND" />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
     )
 }
 
