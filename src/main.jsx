da9e7ed9add3693f7ef48515d4211264b086e475
F. Andrés Villegas <avillegas332@gmail.com>
2022-12-06 20:59:50 -0300

    provide the store with React-Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../src/routes/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import dotenv from 'dotenv';
+import axios from 'axios';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "../src/routes/App";
+import "./index.css";
+import store from './stateManagement/reduxStore.js';
+
+axios.defaults.baseURL = import.meta.BACKEND_URL || "http://localhost:3000";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
+);
