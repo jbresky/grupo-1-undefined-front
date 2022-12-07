09d0bcfa89f45354ce7df18a95e701a629047d20
F. Andrés Villegas <avillegas332@gmail.com>
2022-12-06 21:00:40 -0300

    add component Home

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import Home from './Home';
+import '../App.css'
 
 const App = () => {
     return (
         <ChakraProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path='/' element={"Home page... nothing to see yet"} />
+                    <Route exact path='/' element={<Home/>} />
                     <Route exact path='*' element="NOT FOUND" />
                 </Routes>
             </BrowserRouter>
