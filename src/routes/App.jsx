665f02248f0cddc8881b8482f2e8f275abb3f190
F. Andrés Villegas <avillegas332@gmail.com>
2022-12-06 21:16:08 -0300

    move Home to Pages folder

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import Home from './Home';
+import Home from '../pages/Home.jsx';
 import '../App.css'
 
 const App = () => {
