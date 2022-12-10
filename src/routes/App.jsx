24d32d6bb067c45f1bad5ccb1b97a10a424bb28d
jbresky <joaquin_bresky@hotmail.com>
2022-12-10 16:00:56 -0300

    pages imports - getting rid of App.css

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import Home from '../pages/Home.jsx';
-import Login from '../pages/Login.jsx';
-import '../App.css'
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import Login from '../pages/Login';
 
 const App = () => {
     return (
@@ -11,6 +11,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route exact path='/' element={<Home/>} />
+                    <Route exact path='/profile' element={<Profile/>} />
                     <Route exact path='/login' element={<Login/>} />
                     <Route exact path='*' element="NOT FOUND" />
                 </Routes>
