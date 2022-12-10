851c5c3565b5147784d7bbb7d1b168cee847fda9
jbresky <joaquin_bresky@hotmail.com>
2022-12-10 13:23:24 -0300

    responsiveness: header and profile

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import {Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Box, HStack } from '@chakra-ui/react';
 import HeaderDrawer from '../Drawers/HeaderDrawer';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link } from '@chakra-ui/react';
 
 const Header = () => {
     return (
-        <Flex background={'#2d3748'} justify={'space-between'} p={8}>
+        <Flex
+        alignItems={'center'}
+        background={'gray.800'} 
+        justify={['space-between', 'space-between', 'space-around']} 
+        p={6}
+        >
             <Heading color={'gray.100'}>AlkyBank</Heading>
-            <HeaderDrawer />
+            <Box display={['block', 'block', 'none']}>
+                <HeaderDrawer />
+            </Box>
+                <HStack display={['none', 'none', 'block']}fontSize={'19px'} color={'gray.100'} spacing={12}>
+                    {/* Se podría crear un componente para los links ya que se está repitiendo en el drawer */}
+                    <Link as={RouterLink} to='/profile'>Mi perfil</Link>
+                    <Link as={RouterLink} to='/'>Enviar dinero</Link>
+                    <Link as={RouterLink} to='/'>Movimientos</Link>
+                    <Link as={RouterLink} to='/'>Gastos</Link>
+                </HStack>
         </Flex>
     )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,62 +3,70 @@ import React from 'react'
 import Spacing from '../Components/Spacing'
 import { BiChevronLeft } from 'react-icons/bi'
 import { Link as RouterLink } from 'react-router-dom'
+import Header from '../Components/Layout/Header'
 
 export default function Profile() {
     return (
         <>
-            <Card background={'gray.800'} color={'gray.100'} borderRadius={'0'} p={2}>
+        <Header/>
+            <Card background={'gray.50'} color={'gray.800'} borderRadius={'0'} p={4}>
                
-                <CardHeader pt={3}>
-                <Box pb={4}>
+                <CardHeader w={['95%', '80%', '80%', '60%']} margin={'auto'}>
+                <Box w={'40px'} pb={4}>
                     <Link as={RouterLink} to="/">
                         <BiChevronLeft fontSize={25}/>
                     </Link>
                 </Box>
                     <Flex justify={'space-between'}>
-                        <Heading size='md'>Personal details</Heading>
-                        <Text textDecoration={'underline'}>Edit</Text>
+                        <Heading size={['sm', 'md', 'md', 'lg']}>Personal details</Heading>
+                        <Text fontSize={['sm', 'md', 'md', 'lg']} textDecoration={'underline'}>Edit</Text>
                     </Flex>
                 </CardHeader>
 
-                <CardBody margin={'auto'} background={'gray.700'} w={'95%'} mb={10} borderRadius={'5px'}>
-                    <Stack divider={<StackDivider />} spacing='4'>
+                <CardBody 
+                margin={'auto'} 
+                background={'gray.100'} 
+                color={'gray.800'} 
+                w={['95%', '80%', '80%', '60%']} mb={10} 
+                borderRadius={'5px'}
+                >
+                    <Stack divider={<StackDivider background={'gray.300'} />} spacing='4'>
                         <Box>
-                            <Heading size='xs'>
+                            <Heading size={['sm', 'md', 'md', 'md']}>
                                 Name
                             </Heading>
-                            <Text pt='2' fontSize='sm'>
+                            <Text pt='2' fontSize={['sm', 'md', 'md', 'lg']}>
                                 Name shown
                             </Text>
                         </Box>
                         <Box>
-                            <Heading size='xs'>
+                            <Heading size={['sm', 'md', 'md', 'md']}>
                                 Last name
                             </Heading>
-                            <Text pt='2' fontSize='sm'>
+                            <Text pt='2' fontSize={['sm', 'md', 'md', 'lg']}>
                                 Last name here
                             </Text>
                         </Box>
                         <Box>
-                            <Heading size='xs'>
+                            <Heading size={['sm', 'md', 'md', 'md']}>
                                 Email
                             </Heading>
-                            <Text pt='2' fontSize='sm'>
+                            <Text pt='2' fontSize={['sm', 'md', 'md', 'lg']}>
                                 joaquin@alkemy.com
                             </Text>
                         </Box>
                         <Box>
-                            <Heading size='xs'>
+                            <Heading size={['sm', 'md', 'md', 'md']}>
                                 CUIT
                             </Heading>
-                            <Text pt='2' fontSize='sm'>
+                            <Text pt='2' fontSize={['sm', 'md', 'md', 'lg']}>
                                 20410498244
                             </Text>
                         </Box>
                     </Stack>
                 </CardBody>
             </Card>
-            <Spacing bg={'gray.800'} />
+            {/* <Spacing bg={'gray.800'} /> */}
         </>
     )
 }
