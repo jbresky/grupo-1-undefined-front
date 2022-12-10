5a5c1e84247f7dc5f60cdf6892ce1c271c704aef
jbresky <joaquin_bresky@hotmail.com>
2022-12-10 11:16:31 -0300

    profile route

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import Home from '../pages/Home.jsx';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route exact path='/' element={<Home/>} />
+                    <Route exact path='/profile' element={<Profile/>} />
                     <Route exact path='*' element="NOT FOUND" />
                 </Routes>
             </BrowserRouter>
