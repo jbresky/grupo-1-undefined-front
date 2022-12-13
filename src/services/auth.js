d5310cfeffefed1211bb1a0ca31dbe7e04203f95
jbresky <joaquin_bresky@hotmail.com>
2022-12-12 22:17:14 -0300

    services for auth and requests

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+import localStorage from "../utils/localStorage";
+import { postRequest } from "./http-request";
+
+const login = async (email, password) => {
+    try {
+        const { body } = await postRequest('/auth/login', { email, password })
+
+        localStorage.write('alkybank', body)
+
+        return body
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const logout = () => localStorage.remove('alkybank')
+
+const auth = { login, logout }
+
+export default auth
