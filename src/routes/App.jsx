a8610f850285358e7f9fc72e1b65a696d29cb803
jbresky <joaquin_bresky@hotmail.com>
2022-12-12 22:21:57 -0300

    adding react-query provider

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import Home from '../pages/Home';
-import Profile from '../pages/Profile';
+import Home from '../pages/user/Home';
+import Profile from '../pages/user/Profile';
 import Login from '../pages/Login';
+import Admin from '../pages/admin/Admin';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 const App = () => {
     return (
-        <ChakraProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path='/' element={<Home/>} />
-                    <Route exact path='/profile' element={<Profile/>} />
-                    <Route exact path='/login' element={<Login/>} />
-                    <Route exact path='*' element="NOT FOUND" />
-                </Routes>
-            </BrowserRouter>
-        </ChakraProvider>
+        <QueryClientProvider client={client}>
+            <ChakraProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route exact path='/' element={<Home />} />
+                        <Route exact path='/admin' element={<Admin />} />
+                        <Route exact path='/profile' element={<Profile />} />
+                        <Route exact path='/login' element={<Login />} />
+                        <Route exact path='*' element="NOT FOUND" />
+                    </Routes>
+                </BrowserRouter>
+            </ChakraProvider>
+        </QueryClientProvider>
     )
 }
 
