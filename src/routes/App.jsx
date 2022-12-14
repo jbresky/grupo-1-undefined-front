2cc9e9d49e8971599eb4fa1d359a6010e434f4df
jbresky <joaquin_bresky@hotmail.com>
2022-12-14 19:12:21 -0300

    removing unnecessary files

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from '../pages/user/Home';
 import Profile from '../pages/user/Profile';
-import Login from '../pages/Login';
 import Admin from '../pages/admin/Admin';
 import CreateTransaction from '../pages/CreateTransaction';
 import Transactions from '../pages/Transactions';
 import Register from '../pages/Register';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Signin from '../pages/Signin';
 
 const client = new QueryClient();
 
@@ -20,9 +20,9 @@ const App = () => {
                     <Routes>
                         <Route exact path='/' element={<Home />} />
                         <Route exact path='/profile' element={<Profile />} />
-                        <Route exact path='/login' element={<Login />} />
+                        <Route exact path='/login' element={<Signin />} />
                         <Route exact path='/admin' element={<Admin />} />
-                        <Route exact path='/register' element={<Register action="register" />} />
+                        <Route exact path='/register' element={<Register/>} />
                         <Route exact path='/transactions/create' element={<CreateTransaction />} />
                         <Route exact path='/transactions/' element={<Transactions />} />
                         <Route exact path='*' element="NOT FOUND" />
