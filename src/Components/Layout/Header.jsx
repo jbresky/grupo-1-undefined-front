233b8f74b091c30535bb66a76e438c758a59c61a
jbresky <joaquin_bresky@hotmail.com>
2022-12-15 05:17:05 -0300

    header layout - show links and actions for each role

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,32 +1,53 @@
 import React from 'react'
-import { Flex, Heading, Box, HStack } from '@chakra-ui/react';
+import { Flex, Heading, Box, HStack, Button } from '@chakra-ui/react';
 import HeaderDrawer from '../Drawers/HeaderDrawer';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Link } from '@chakra-ui/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { authLogout } from '../../app/features/authSlice';
 
 const Header = () => {
+
+    const { user } = useSelector(state => state.auth)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        dispatch(authLogout())
+        navigate('/login')
+    }
+    
     return (
         <Flex
-        alignItems={'center'}
-        background={'gray.800'} 
-        justify={['space-between', 'space-between', 'space-around']} 
-        p={6}
+            alignItems={'center'}
+            background={'gray.800'}
+            justify={['space-between', 'space-between', 'space-around']}
+            p={6}
         >
             <Heading color={'gray.100'}>
                 <Link textDecoration={'none'} as={RouterLink} to='/'>
-                AlkyBank
+                    AlkyBank
                 </Link>
-                </Heading>
+            </Heading>
             <Box display={['block', 'block', 'none']}>
                 <HeaderDrawer />
             </Box>
-                <HStack display={['none', 'none', 'block']}fontSize={'19px'} color={'gray.100'} spacing={12}>
-                    {/* Se podría crear un componente para los links ya que se está repitiendo en el drawer */}
-                    <Link as={RouterLink} to='/transactions/create'>Enviar dinero</Link>
-                <Link as={RouterLink} to='/transactions'>Movimientos</Link>
-                <Link as={RouterLink} to='/login'>Log In</Link>
-                <Link as={RouterLink} to='/register'>Register</Link>
-                </HStack>
+            <HStack display={['none', 'none', 'block']} fontSize={'19px'} color={'gray.100'} spacing={12}>
+                {user ? (
+                    <>
+                        <Link as={RouterLink} to='/transactions'>Movimientos</Link>
+                        <Link as={RouterLink} to='/transactions/create'>Enviar dinero</Link>
+                        <Button color={'black'} onClick={handleLogout}>Logout</Button>
+                    </>
+                ) :
+                    <>
+                        <Link as={RouterLink} to='/login'>Log In</Link>
+                        <Link as={RouterLink} to='/register'>Register</Link>
+
+                    </>
+                }
+            </HStack>
         </Flex>
     )
 }
