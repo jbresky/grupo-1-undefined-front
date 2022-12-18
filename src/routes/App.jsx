5c76bb5ef0ab87bf733ffca78967789624d4f085
jbresky <joaquin_bresky@hotmail.com>
2022-12-18 00:16:24 -0300

    protected routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from '../pages/user/Home';
 import Profile from '../pages/user/Profile';
@@ -11,28 +11,30 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Signin from '../pages/Signin';
 import UserDetail from '../pages/admin/UserDetail';
 import Avatar from '../pages/user/Avatar';
-
+import { isLogged, isLoggedAdmin } from '../utils/isLogged';
 const client = new QueryClient();
 
 const App = () => {
+    const loggedAdmin = isLoggedAdmin()
+    const logged = isLogged()
+
     return (
         <QueryClientProvider client={client}>
             <ChakraProvider>
                 <BrowserRouter>
                     <Routes>
-                        <Route exact path='/' element={<Home />}>
-                            <Route path=':transaction' element={<CreateTransaction />}></Route>
+                        <Route path='/login' element={!logged ? <Signin /> : <Navigate to='/' />} />
+                        <Route exact path='/register' element={!logged ? <Register /> : <Navigate to='/' />} />
+                        <Route path='/' element={logged ? <Home /> : <Navigate to='/login' />}>
+                            <Route path=':transaction' element={<CreateTransaction />} />
                         </Route>
+                        <Route exact path='/transactions/create' element={<CreateTransaction />} />
                         <Route exact path='/profile' element={<Profile />}>
                             <Route path=':avatar' element={<Avatar />}></Route>
                         </Route>
-                        <Route exact path='/login' element={<Signin />} />
-                        <Route exact path='/admin' element={<Admin />} >
+                        <Route path='/admin' element={loggedAdmin ? <Admin /> : <Navigate to='/' />} >
                             <Route path=':user:id' element={<UserDetail />} />
                         </Route>
-                        <Route exact path='/register' element={<Register />} />
-                        {/* <Route exact path='/transactions/create' element={<CreateTransaction />} /> */}
-                        <Route exact path='/transactions' element={<Transactions />} />
                         <Route exact path='*' element="NOT FOUND" />
                     </Routes>
                 </BrowserRouter>
