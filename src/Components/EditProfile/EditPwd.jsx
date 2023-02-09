37474f6f36cec322ee122ae784f92f6c15e81c08
jbresky <joaquin_bresky@hotmail.com>
2023-02-09 00:43:09 -0500

    edit password component

--- src/Components/EditProfile/EditPwd.jsx
+++ src/Components/EditProfile/EditPwd.jsx
@@ -1,15 +1,54 @@
 import { Formik } from "formik";
 import { useSelector } from "react-redux";
 import { useToast, Alert, AlertIcon } from "@chakra-ui/react";
-import { updateUserPassword } from "../../app/actions/authActions";
+// import { updateUserPassword } from "../../app/actions/authActions";
 import { editPwdSchema } from "../YupValidator/schemas";
 import EditPwdContainer from "./EditPwdContainer";
+import { Api } from "../../api";
 
 export default function EditProfilePwd() {
 
+    const { user } = useSelector(state => state.auth);
+
     const toast = useToast();
 
-    const { user } = useSelector(state => state.auth);
+    const success = () => toast({
+        duration: 3000,
+        isClosable: true,
+        render: () => (
+            <Alert status="success">
+                <AlertIcon />
+                Password successfully updated
+            </Alert>
+        ),
+        position: "top",
+    });
+
+    const error = () => toast({
+        duration: 3000,
+        isClosable: true,
+        render: () => (
+            <Alert status="error">
+                <AlertIcon />
+                Incorrect password
+            </Alert>
+        ),
+        position: "top",
+    });
+
+    async function handleSend({ password, newPassword }) {
+        try {
+            const service = new Api();
+            const response = await service.apiPrivate().put(`/users/password/${user.id}`, { password, newPassword });
+            if (response.data.body) {
+                success()
+            } else {
+                error()
+            }
+        } catch (erorr) {
+            error()
+        }
+    }
 
     return (
         <div>
@@ -18,59 +57,29 @@ export default function EditProfilePwd() {
                 newPassword: '',
                 repeatPassword: ''
             }}
-            validationSchema={editPwdSchema}
-            onSubmit={values => {
-                const response = updateUserPassword(user.id, {
+                validationSchema={editPwdSchema}
+                onSubmit={values => handleSend({
                     password: values.currentPassword,
                     newPassword: values.newPassword
-                })
-                response.then(res => {
-                    if(res.status === 200){
-                        toast({
-                            status: res.statusText,
-                            duration: 3000,
-                            isClosable: true,
-                            render: () => (
-                                <Alert status="success">
-                                    <AlertIcon/>
-                                    Your password has been changed successfully
-                                </Alert>
-                            ),
-                            position: "top",
-                        });
-                    } else {
-                        toast({
-                            duration: 3000,
-                            isClosable: true,
-                            render: () => (
-                              <Alert status="error">
-                                <AlertIcon />
-                                Not possible to edit password
-                              </Alert>
-                            ),
-                            position: "top",
-                        });
-                    }
-                })
-            }}
+                })}
             >
-            {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit
-            }) => (
-                <EditPwdContainer
-                values={values}
-                handleSubmit={handleSubmit}
-                handleChange={handleChange}
-                handleBlur={handleBlur}
-                touched={touched}
-                errors={errors}
-                />
-            )}
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit
+                }) => (
+                    <EditPwdContainer
+                        values={values}
+                        handleSubmit={handleSubmit}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        touched={touched}
+                        errors={errors}
+                    />
+                )}
             </Formik>
         </div>
     )
